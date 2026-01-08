3bd54f1987f885e05d288c13ad019687a929529c
Gabriel Zemuner <gabrielzemuner@live.com>
2026-01-08 00:58:28 -0300

    inclusao pagina metodos de array, inclusao react e tailwindcss com um modal de exemplo codigo js puro vs react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,39 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://gabrielzemuner.github.io",
-  base: "estudocs/",
-  integrations: [
-    starlight({
-      title: "EstuDocs",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/withastro/starlight",
-        },
-      ],
+  base: "/estudocs/",
 
-      // 👇 isso habilita a sidebar “dinâmica” por rota
-      routeMiddleware: "./src/routeData.ts",
+  integrations: [starlight({
+    title: "EstuDocs",
+    customCss: ["./src/styles/global.css"],
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/withastro/starlight",
+      },
+    ],
 
-      // 👇 aqui ficam os “guias” (cada um puxa sua própria pasta)
-      sidebar: [
-        {
-          label: "JS antes do React",
-          autogenerate: { directory: "js-antes-do-react" },
-        },
-        { label: "TypeScript", autogenerate: { directory: "typescript" } },
-      ],
-    }),
-  ],
+    // 👇 isso habilita a sidebar “dinâmica” por rota
+    routeMiddleware: "./src/routeData.ts",
+
+    // 👇 aqui ficam os “guias” (cada um puxa sua própria pasta)
+    sidebar: [
+      {
+        label: "JS antes do React",
+        autogenerate: { directory: "js-antes-do-react" },
+      },
+      { label: "TypeScript", autogenerate: { directory: "typescript" } },
+    ],
+  }), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
